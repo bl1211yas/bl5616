e19b55edbf5433b12677f0bfe170be1d057d3e85
bl1211yas <108929653+bl1211yas@users.noreply.github.com>
2022-07-08 10:59:53 +0100

    Add files via upload

---
+++ precache-manifest.2474f35e6fb3f269781e88ccd60fa389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b1a25cda0c946cfd28",
+    "url": "/static/css/main.d2c05451.chunk.css"
+  },
+  {
+    "revision": "52b1a25cda0c946cfd28",
+    "url": "/static/js/main.52b1a25c.chunk.js"
+  },
+  {
+    "revision": "f693b940d040932ca9d6",
+    "url": "/static/css/1.c48764a9.chunk.css"
+  },
+  {
+    "revision": "f693b940d040932ca9d6",
+    "url": "/static/js/1.f693b940.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ab8e6bdbbfcf53b7504005078ae8e8a",
+    "url": "/index.html"
+  }
+];
